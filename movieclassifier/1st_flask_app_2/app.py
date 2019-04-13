eb12ef4f7d0153769e2d849282e5d5241485fe5f
Jaxon280 <keresu0720@icloud.com>
2019-04-13 15:02:41 +0900

    Done

--- movieclassifier/1st_flask_app_2/app.py
+++ movieclassifier/1st_flask_app_2/app.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from wtforms import Form, TextAreaField, validators
+import importlib
 
 app = Flask(__name__)
 
+class HelloForm(Form):
+    sayhello = TextAreaField('',[validators.DataRequired()])
+
 @app.route('/')
 def index():
-    return render_template('first_app.html')
+    form = HelloForm(request.form)
+    return render_template('first_app.html', form=form)
+
+@app.route('/hello',methods=['POST'])
+def hello():
+    form = HelloForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.sayhello.data
+        return render_template('hello.html',name=name)
+    return render_template('first_app.html', form=form)
 
 if __name__ == '__main__':
     app.run()
